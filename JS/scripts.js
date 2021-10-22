07a0369b9c8b87f33e2397fef737d663c392a2a0
Aaron Ewert <aaronjewert@gmail.com>
2021-10-22 12:39:45 -0700

    add functions for alerts when each button is pushed

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,33 @@
-<script>
-  function myFunctionYes() {
+  function myFunctionNew() {
     alert("You should learn Python!")
   }
-  function myFunctionNo() {
+  function myFunctionOld() {
     alert("You should learn Ruby!")
   }
-</script>
+  function myFunctionLarge() {
+    alert("You should learn C#!")
+  }
+  function myFunctionSmall() {
+    alert("You should learn Python! ")
+  }
+  function myFunctionApps() {
+    alert("Focus on Java")
+  }
+  function myFunctionWeb() {
+    alert("Focus on Ruby")
+  }
+  function myFunctionMicrosoft() {
+    alert("Microsoft loves C#")
+  }
+  function myFunctionAndroid() {
+    alert("Android loves Java")
+  }
+  function myFunctionApple() {
+    alert("Apple created and uses Swift")
+  }
+  function myFunctionDogs() {
+    alert("Dogs love Python")
+  }
+  function myFunctionCats() {
+    alert("Cats love Java")
+  }
